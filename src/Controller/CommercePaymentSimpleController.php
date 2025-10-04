13158ca423bbc2ed6ddb2d0cefc9f90dfcf959e9
kouwa stephane <kksasteph888@gmail.com>
2025-10-04 14:13:14 +0200

    add base style

--- src/Controller/CommercePaymentSimpleController.php
+++ src/Controller/CommercePaymentSimpleController.php
@@ -5,6 +5,8 @@ namespace Drupal\commerce_payment_simple\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\commerce_product\Entity\Product;
+use Drupal\commerce_product\Entity\ProductVariation;
+use Stephane888\Debug\debugLog;
 
 /**
  * Returns responses for Commerce Payment Simple routes.
@@ -14,17 +16,31 @@ final class CommercePaymentSimpleController extends ControllerBase {
   /**
    * Payement en un seule etape.
    */
-  public function paymentOneStep(int $product_id): array {
+  public function paymentOneStep(int $product_variation_id): array {
     /**
      *
-     * @var Product $product
+     * @var ProductVariation $productVariation
      */
-    $product = Product::load($product_id);
+    $productVariation = ProductVariation::load($product_variation_id);
+    if (!$productVariation) {
+      $this->messenger()->addWarning("Le produit n'est plus disponible à la vente");
+      $this->getLogger('commerce_payment_simple')->warning("Tentative d'achat d'un produit inexsistant");
+      debugLog::symfonyDebug($_SERVER, 'commerce_payment_simple', true);
+      return $this->redirect('<front>');
+    }
+    $product = $productVariation->getProduct();
+    $form = $this->formBuilder()->getForm("Drupal\commerce_payment_simple\Form\PaymentStripeForm");
     $build['content'] = [
-      '#type' => 'item',
-      '#markup' => $this->t('It works!')
+      '#theme' => 'commerce_payment_simple_payment_one_step',
+      '#form' => $form,
+      '#product' => $product,
+      '#product_variation' => $productVariation,
+      '#attached' => [
+        'library' => [
+          'commerce_payment_simple/stripe'
+        ]
+      ]
     ];
-    
     return $build;
   }
   
